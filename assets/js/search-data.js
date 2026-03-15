624b70bd1026c5c4f64d7bffa3df22d03a46b951
zhenyan-xing <18983900988@163.com>
2026-03-15 07:11:55 +0000

    Deploying to gh-pages from @ zhenyan-xing/zhenyan-xing.github.io@2bc30fe4ec3ee95e537a38761506febdc10f4b8f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/books/";
               },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
             },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
