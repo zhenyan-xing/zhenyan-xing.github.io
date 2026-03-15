5489c6f978e52eb493ea68c7ab2eafacc6e3ea26
zhenyan-xing <18983900988@163.com>
2026-03-15 05:04:31 +0000

    Deploying to gh-pages from @ zhenyan-xing/zhenyan-xing.github.io@507bedadbe005b418207f3cdc87c10be7183dab1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,11 +440,6 @@ ninja.data = [{
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{id: "projects-rfdiffusion-based-de-novo-symmetric-protein-design",
           title: 'RFdiffusion-based _de novo_ symmetric protein design',
           description: "the work was done in sustech under the supervision of Prof. Zhe Li",
