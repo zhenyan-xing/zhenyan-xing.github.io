cb3556ebc27ffab0bb35776ad2c6e7b6472c0152
zhenyan-xing <18983900988@163.com>
2026-03-15 03:13:25 +0000

    Deploying to gh-pages from @ zhenyan-xing/zhenyan-xing.github.io@508532f2f443d13e73519474526a8f26ac4b5a0e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,9 +9,9 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+  },{id: "nav-research-projects",
+          title: "research projects",
+          description: "A growing collection of my interesting de novo protein design projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
