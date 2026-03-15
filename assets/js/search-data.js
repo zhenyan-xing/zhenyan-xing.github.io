5c0453862545f759dc7aa8e46da8cce62b76b0d1
zhenyan-xing <18983900988@163.com>
2026-03-15 07:52:22 +0000

    Deploying to gh-pages from @ zhenyan-xing/zhenyan-xing.github.io@245c37624017e7faa340efd9d7e39085326f640e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This is the CV page of Zhenyan Xing",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
