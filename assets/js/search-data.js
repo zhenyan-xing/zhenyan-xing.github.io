3231ee462dce0960bcfb11200de62a1ab483dfdf
zhenyan-xing <18983900988@163.com>
2026-03-15 05:05:20 +0000

    Deploying to gh-pages from @ zhenyan-xing/zhenyan-xing.github.io@0388aebf15c4e37ff5db84522ac5ee97f46b4eee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -425,11 +425,6 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
