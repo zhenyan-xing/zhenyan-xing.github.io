909952c57ef38ea2c509368a61e52dd809f05a9e
zhenyan-xing <18983900988@163.com>
2026-05-30 14:50:13 +0000

    Deploying to gh-pages from @ zhenyan-xing/zhenyan-xing.github.io@4c02bc1223174bc81e2a5f39b2816d3a5182dd90 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -418,9 +418,9 @@ ninja.data = [{
           description: "Exploring hallucination and model-inversion strategies for protein–RNA binder design.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Hallucination%20Co-Design%20of%20Protein%E2%80%93RNA%20Complexes%20/";
-            },},{id: "projects-",
-          title: '',
-          description: "",
+            },},{id: "projects-helixlinkerlab-a-automatic-pipline-for-protein-linker-design",
+          title: 'HelixLinkerLab: a automatic pipline for protein linker design',
+          description: "An automatic pipline to design rigid helical linkers for cryo-EM using structure prediction models and inverse folding",
           section: "Projects",handler: () => {
               window.location.href = "/projects/HelixLinkerLab/";
             },},{id: "projects-rfdiffusion-based-de-novo-symmetric-protein-design",
